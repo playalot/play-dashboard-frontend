d9a9b36c813cbe5cfdab51018d2b2dec47df6ec3
15620693528 <2363140194@qq.com>
2016-12-30 14:04:45 +0800

    标签类别

--- src/scripts/actions/tagClassAction.js
+++ src/scripts/actions/tagClassAction.js
@@ -1,6 +1,7 @@
 import Request from 'superagent'
 
 export const RECEIVE_TAG_CLASS = 'RECEIVE_TAG_CLASS'
+export const RECEIVE_TOY_CLASS = 'RECEIVE_TOY_CLASS'
 
 function receiveTagClass(res) {
     return {
@@ -9,12 +10,28 @@ function receiveTagClass(res) {
     }
 }
 
+// function receiveToyClass(res) {
+//     return {
+//         type: RECEIVE_TOY_CLASS,
+//         res
+//     }
+// }
 export function fetchTagClass() {
     return function(dispatch) {
         return Request
             .get(`/api/classifications`)
             .end(function(err,res){
-                dispatch(receiveTagClass(res.body))
+                dispatch(receiveTagClass(res.body.tags))
             })
     }
 }
+
+// export function fetchToyClass() {
+//     return function(dispatch) {
+//         return Request
+//             .get(`/api/classifications`)
+//             .end(function(err,res){
+//                 dispatch(receiveToyClass(res.body.toys))
+//             })
+//     }
+// }

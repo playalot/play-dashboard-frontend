320cf48f488cda3da0183ac02f1a5edbfc37de9c
15620693528 <2363140194@qq.com>
2017-02-09 18:41:37 +0800

    git bug 重新提交

--- src/scripts/components/OrderList/OrderList.jsx
+++ src/scripts/components/OrderList/OrderList.jsx
@@ -162,6 +162,7 @@ export default class OrderList extends Component{
 							  <img style={{width:'30px'}} src={order.user.avatar}  data-toggle="dropdown" className="img-circle"/>
 							  <ul className="dropdown-menu">
 							    <li><a onClick={() => this.context.router.push(`/user/${order.user.id}`)}>查看</a></li>
+							    <li><a onClick={() => this.props.setTouid(order.user.id,order.user.avatar)}>私信</a></li>
 							  </ul>
 							</div>
 	                      </td>

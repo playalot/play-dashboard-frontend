7ec0db25a9ad78b713b28cdb8e59dd7985df0db3
DsAco <2363140194@qq.com>
2017-10-24 15:58:39 +0800

    用户详情页 注销，通知

--- src/scripts/components/UserDetail/UserDetail.jsx
+++ src/scripts/components/UserDetail/UserDetail.jsx
@@ -20,11 +20,17 @@ export default class extends Component{
 			type:'master',
 			user:{},
 			currentPage:'posts',
-			postPage:0
+			postPage:0,
+			replyContent:'',
 		}
 		this.setActive = this._setActive.bind(this)
 		this.approve = this._approve.bind(this)
 		this.goPage = this._goPage.bind(this)
+		this.sendReply = this._sendReply.bind(this)
+		this.showReplyModal = () => {
+			$('#replyModal').modal('show')
+		}
+		this.zhuxiao = this._zhuxiao.bind(this)
 	}
 	componentWillMount() {
 		if(!this.props.classLoaded){
@@ -38,6 +44,13 @@ export default class extends Component{
 	componentWillUnmount() {
 		this.props.clearPost()
 		this.props.clearPage()
+	}
+	componentDidMount() {
+		$('#replyModal').on('hidden.bs.modal', (e) => {
+			this.setState({
+				replyContent:''
+			})
+		})
 	}
 	_approve() {
 		const { userId,note,type } = this.state
@@ -61,6 +74,33 @@ export default class extends Component{
 		this.setState({ postPage })
 		this.props.getUserPost(this.state.userId,postPage)
 	}
+	_sendReply() {
+		const { replyContent,userId } = this.state
+		Request.post(`/api/user/${userId}/reply`)
+		.send({
+			content:replyContent
+		})
+		.end((err,res) => {
+			if(err) {
+				Toastr.error(`通知失败。`)
+			}else{
+				Toastr.success(`已通知～`)
+				$('#replyModal').modal('hide')
+			}
+		})
+	}
+	_zhuxiao() {
+		if (confirm('确定注销吗?')) {
+			Request.post(`/api/user/${this.state.userId}/remove`)
+			.end((err,res) => {
+				if(err) {
+					Toastr.error(`注销用户失败。`)
+				}else{
+					Toastr.success(`注销用户成功`)
+				}
+			})
+		}
+	}
 	render() {
 		const { dialogApprove,note,type } = this.state
 		const { user } = this.props
@@ -74,15 +114,15 @@ export default class extends Component{
 								<img className="avatar120" src={user.avatar} alt="User Avatar" />
 								<h4 className="text-info my-3"><strong>{ user.nickname }</strong></h4>
 								<div className="row">
-									<div className="col-4">
+									<div className="col-4 text-center">
 										<h4 className="text-primary"><strong>{user.counts.posts}</strong></h4>
 										<strong className="text-muted">图片</strong>
 									</div>
-									<div className="col-4">
+									<div className="col-4 text-center">
 										<h4 className="text-primary"><strong>{user.counts.followers}</strong></h4>
 										<strong className="text-muted">粉丝</strong>
 									</div>
-									<div className="col-4">
+									<div className="col-4 text-center">
 										<h4 className="text-primary"><strong>{user.counts.following}</strong></h4>
 										<strong className="text-muted">关注</strong>
 									</div>
@@ -100,7 +140,7 @@ export default class extends Component{
 									<span style={{marginRight:15}}>{user.verify.type}</span>
 									:null
 								}
-								<button onClick={() => this.setState({dialogApprove:true})} className="btn btn-sm btn-primary">
+								<button onClick={() => this.setState({dialogApprove:true})} className="btn btn-sm btn-success">
 									{
 										user.verify && user.verify.type ? '修改' : '添加'
 									}
@@ -110,7 +150,7 @@ export default class extends Component{
 						<div className="row">
 							<label className="col-3 col-form-label font-weight-bold">Level</label>
 							<div className=" col-9 d-flex align-items-center">
-								<span className="badge badge-warning">LV {user.level}</span>
+								<span className="badge badge-warning text-white">LV {user.level}</span>
 							</div>
 						</div>
 						<div className="row">
@@ -144,13 +184,15 @@ export default class extends Component{
 						<div className="row">
 							<label className="col-3 col-form-label font-weight-bold">Chat</label>
 							<div className=" col-9 d-flex align-items-center">
-								<button className="btn btn-sm btn-primary" onClick={() => this.props.setTouid(user.id,user.avatar)}>私信</button>
+								<button className="btn btn-sm btn-primary mr-1" onClick={() => this.props.setTouid(user.id,user.avatar)}>私信</button>
+								<button className="btn btn-sm btn-primary" onClick={this.showReplyModal}>通知</button>
 							</div>
 						</div>
 						<div className="row">
 							<label className="col-3 col-form-label font-weight-bold">Active</label>
 							<div className=" col-9 d-flex align-items-center">
-								<button onClick={this.setActive} className="btn btn-sm btn-danger"><i className="fa fa-eye-slash">&nbsp;</i>{user.isActive?'ban':'activate'}</button>
+								<button onClick={this.setActive} className="btn btn-sm btn-warning mr-1"><i className="fa fa-eye-slash">&nbsp;</i>{user.isActive?'ban':'activate'}</button>
+								<button onClick={this.zhuxiao} className="btn btn-sm btn-danger">注销</button>
 							</div>
 						</div>
 					</div>
@@ -159,24 +201,24 @@ export default class extends Component{
 					<div className="d-flex justify-content-end align-items-center">
 						<ul style={{borderBottom:'transparent'}} className="nav nav-tabs m-tabs-line m-tabs-line--2x m-tabs-line--info" role="tablist">
 							<li className="nav-item m-tabs__item">
-								<a className="nav-link m-tabs__link active" data-toggle="tab" href="#posts" role="tab">
+								<a className="nav-link m-tabs__link active" data-toggle="tab" href="#user_posts" role="tab">
 									图片
 								</a>
 							</li>
 							<li className="nav-item m-tabs__item">
-								<a className="nav-link m-tabs__link" data-toggle="tab" href="#pages" role="tab">
+								<a className="nav-link m-tabs__link" data-toggle="tab" href="#user_pages" role="tab">
 									文章
 								</a>
 							</li>
 						</ul>
 					</div>
 					<div className="tab-content">
-						<div className="tab-pane active" id="posts" role="tabpanel">
+						<div className="tab-pane active" id="user_posts" role="tabpanel">
 							<PostPanels/>
 							<ReactPaginate 
 								previousLabel={<span>&laquo;</span>}
 								nextLabel={<span>&raquo;</span>}
-								breakLabel={<span>...</span>}
+								breakLabel={<a>...</a>}
 								breakClassName={"break-me"}
 								pageCount={this.props.totalPages}
 								marginPagesDisplayed={2}
@@ -187,7 +229,7 @@ export default class extends Component{
 								forcePage={this.state.postPage}
 								activeClassName={"active"} />
 						</div>
-						<div className="tab-pane" id="pages" role="tabpanel">
+						<div className="tab-pane" id="user_pages" role="tabpanel">
 							<PagePanel/>
 						</div>
 					</div>
@@ -216,6 +258,24 @@ export default class extends Component{
 					</div>
 					: null
 				}
+				<div className="modal fade" id="replyModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="exampleModalLabel">通知内容</h5>
+									<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div className="modal-body">
+								<textarea value={this.state.replyContent} onChange={(e) => this.setState({replyContent:e.target.value})} className="w-100" name="" id="" cols="30" rows="5"></textarea>
+							</div>
+							<div className="modal-footer">
+								<button onClick={this.sendReply} type="button" className="btn btn-outline-primary">发送</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
